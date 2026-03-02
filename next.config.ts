8fbae9920e3caa0795aebbc865aa229f206d688c
badwymt <badwy.mt@gmail.com>
2026-03-01 16:55:57 -0800

    fix: add /ramadan rewrite to handle lowercase URL

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    async rewrites() {
+          return [
+            {
+                      source: "/ramadan",
+                      destination: "/Ramadan",
+            },
+                ];
+    },
 };
 
 export default nextConfig;
